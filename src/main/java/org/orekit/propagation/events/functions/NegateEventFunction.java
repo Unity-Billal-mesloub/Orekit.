Unity-Billal-mesloub/Orekit

add NegateEventFunction

src/main/java/org/orekit/propagation/events/functions/NegateEventFunction.java
@@ -1,0 +1,55 @@
+/* Copyright 2022-2025 Romain Serra
+ * Licensed to CS GROUP (CS) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * CS licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.orekit.propagation.events.functions;
+
+import org.hipparchus.CalculusFieldElement;
+import org.orekit.propagation.FieldSpacecraftState;
+import org.orekit.propagation.SpacecraftState;
+
+/** Negated event function.
+ * @author Romain Serra
+ * @since 14.0
+ */
+public class NegateEventFunction implements EventFunctionModifier {
+
+    /** Original event function. */
+    private final EventFunction baseFunction;
+
+    /**
+     * Constructor.
+     * @param baseFunction event function to negate
+     */
+    public NegateEventFunction(final EventFunction baseFunction) {
+        this.baseFunction = baseFunction;
+    }
+
+    @Override
+    public EventFunction getBaseFunction() {
+        return baseFunction;
+    }
+
+    @Override
+    public double value(final SpacecraftState state) {
+        return -baseFunction.value(state);
+    }
+
+    @Override
+    public <T extends CalculusFieldElement<T>> T value(final FieldSpacecraftState<T> fieldState) {
+        return baseFunction.value(fieldState).negate();
+    }
+
+}

Unity-Billal-mesloub/Orekit.

src/main/java/org/orekit/propagation/events/functions/NegateEventFunction.java
@@ -1,0 +1,55 @@
+/* Copyright 2022-2025 Romain Serra
+ * Licensed to CS GROUP (CS) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * CS licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.orekit.propagation.events.functions;
+
+import org.hipparchus.CalculusFieldElement;
+import org.orekit.propagation.FieldSpacecraftState;
+import org.orekit.propagation.SpacecraftState;
+
+/** Negated event function.
+ * @author Romain Serra
+ * @since 14.0
+ */
+public class NegateEventFunction<S extends EventFunction> implements EventFunctionModifier {
+
+    /** Original event function. */
+    private final S baseFunction;
+
+    /**
+     * Constructor.
+     * @param baseFunction event function to negate
+     */
+    public NegateEventFunction(final S baseFunction) {
+        this.baseFunction = baseFunction;
+    }
+
+    @Override
+    public S getBaseFunction() {
+        return baseFunction;
+    }
+
+    @Override
+    public double value(final SpacecraftState state) {
+        return -baseFunction.value(state);
+    }
+
+    @Override
+    public <T extends CalculusFieldElement<T>> T value(final FieldSpacecraftState<T> fieldState) {
+        return baseFunction.value(fieldState).negate();
+    }
+
+}